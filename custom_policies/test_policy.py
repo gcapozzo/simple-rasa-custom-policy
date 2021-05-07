cdeddd736e551727760b7ef55df1bcacc8917807
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-05-07 09:55:38 -0300

    the bot gives a default answer

---
+++ custom_policies/test_policy.py
@@ -0,0 +1,79 @@
+import zlib
+
+import base64
+import json
+import logging
+
+from tqdm import tqdm
+from typing import Optional, Any, Dict, List, Text
+
+import rasa.utils.io
+import rasa.shared.utils.io
+from rasa.shared.constants import DOCS_URL_POLICIES
+from rasa.shared.core.domain import State, Domain
+from rasa.shared.core.events import ActionExecuted
+from rasa.core.featurizers.tracker_featurizers import (
+    TrackerFeaturizer,
+    MaxHistoryTrackerFeaturizer,
+)
+from rasa.shared.nlu.interpreter import NaturalLanguageInterpreter
+from rasa.core.policies.policy import Policy, PolicyPrediction
+from rasa.shared.core.trackers import DialogueStateTracker
+from rasa.shared.core.generator import TrackerWithCachedStates
+from rasa.shared.utils.io import is_logging_disabled
+from rasa.core.constants import MEMOIZATION_POLICY_PRIORITY
+
+logger = logging.getLogger(__name__)
+
+# temporary constants to support back compatibility
+MAX_HISTORY_NOT_SET = -1
+OLD_DEFAULT_MAX_HISTORY = 5
+
+class TestPolicy(Policy):
+    def train(
+        self,
+        training_trackers: List[TrackerWithCachedStates],
+        domain: Domain,
+        interpreter: NaturalLanguageInterpreter,
+        **kwargs: Any,
+    ) -> None:
+        """Trains the policy on given training trackers.
+
+        Args:
+            training_trackers:
+                the list of the :class:`rasa.core.trackers.DialogueStateTracker`
+            domain: the :class:`rasa.shared.core.domain.Domain`
+            interpreter: Interpreter which can be used by the polices for featurization.
+        """
+        pass
+    def predict_action_probabilities(
+        self,
+        tracker: DialogueStateTracker,
+        domain: Domain,
+        interpreter: NaturalLanguageInterpreter,
+        **kwargs: Any,
+    ) -> PolicyPrediction:
+        result = self._default_predictions(domain)
+        result[13] = 1.0
+        print(result)
+        return self._prediction(result)
+        """Predicts the next action the bot should take after seeing the tracker.
+
+        Args:
+            tracker: the :class:`rasa.core.trackers.DialogueStateTracker`
+            domain: the :class:`rasa.shared.core.domain.Domain`
+            interpreter: Interpreter which may be used by the policies to create
+                additional features.
+
+        Returns:
+             The policy's prediction (e.g. the probabilities for the actions).
+        """
+
+    def _metadata(self) -> Dict[Text, Any]:   
+        return {
+            "priority": self.priority,
+        }
+        
+    @classmethod
+    def _metadata_filename(cls) -> Text:
+        return "test_policy.json"
