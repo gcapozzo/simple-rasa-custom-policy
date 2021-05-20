fcac6f05c498936a341f1b1fc1e18cfc346bff19
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-05-20 19:11:32 -0300

    create profiles from stories

--- custom_policies/test_policy.py
+++ custom_policies/test_policy.py
@@ -11,13 +11,13 @@ import rasa.utils.io
 import rasa.shared.utils.io
 from rasa.shared.constants import DOCS_URL_POLICIES
 from rasa.shared.core.domain import State, Domain
-from rasa.shared.core.events import ActionExecuted
+from rasa.shared.core import events
 from rasa.core.featurizers.tracker_featurizers import (
     TrackerFeaturizer,
     MaxHistoryTrackerFeaturizer,
 )
 from rasa.shared.nlu.interpreter import NaturalLanguageInterpreter
-from rasa.core.policies.policy import Policy, PolicyPrediction
+from rasa.core.policies.policy import Policy, PolicyPrediction, confidence_scores_for
 from rasa.shared.core.trackers import DialogueStateTracker
 from rasa.shared.core.generator import TrackerWithCachedStates
 from rasa.shared.utils.io import is_logging_disabled
@@ -29,24 +29,43 @@ logger = logging.getLogger(__name__)
 MAX_HISTORY_NOT_SET = -1
 OLD_DEFAULT_MAX_HISTORY = 5
 
+
 class TestPolicy(Policy):
     def __init__(
-        self,
-        featurizer: Optional[TrackerFeaturizer] = None,
-        priority: int = MEMOIZATION_POLICY_PRIORITY,
-        answered: Optional[bool] = None,
-        **kwargs: Any,
-        ) -> None:
+            self,
+            featurizer: Optional[TrackerFeaturizer] = None,
+            priority: int = MEMOIZATION_POLICY_PRIORITY,
+            usertype: Optional[float] = None,
+            story_profiles: Optional[List] = None,
+            **kwargs: Any,
+    ) -> None:
         super().__init__(featurizer, priority, **kwargs)
-        self.answered = False
+        self.story_profiles = story_profiles if story_profiles is not None else []
+        self.usertype = 0.0
 
     def train(
-        self,
-        training_trackers: List[TrackerWithCachedStates],
-        domain: Domain,
-        interpreter: NaturalLanguageInterpreter,
-        **kwargs: Any,
+            self,
+            training_trackers: List[TrackerWithCachedStates],
+            domain: Domain,
+            interpreter: NaturalLanguageInterpreter,
+            **kwargs: Any,
     ) -> None:
+        # only original stories
+        training_trackers = [
+            t
+            for t in training_trackers
+            if not hasattr(t, "is_augmented") or not t.is_augmented
+        ]
+        for s in training_trackers:
+            # initialize dict with intents as keys and 0 counts in each history
+            story_intents = dict.fromkeys(domain.intents, 0)
+            for t in s.events:
+                if isinstance(t, events.UserUttered):
+                    intent = t.as_dict().get('parse_data').get('intent').get('name')
+                    story_intents[intent] = story_intents[intent] + 1
+            print(story_intents)
+            self.story_profiles.append(story_intents)
+            # todo: transform results to probabilities
         """Trains the policy on given training trackers.
 
         Args:
@@ -58,39 +77,27 @@ class TestPolicy(Policy):
         pass
 
     def predict_action_probabilities(
-        self,
-        tracker: DialogueStateTracker,
-        domain: Domain,
-        interpreter: NaturalLanguageInterpreter,
-        **kwargs: Any,
+            self,
+            tracker: DialogueStateTracker,
+            domain: Domain,
+            interpreter: NaturalLanguageInterpreter,
+            **kwargs: Any,
     ) -> PolicyPrediction:
-        result = self._default_predictions(domain)
-        tracker.past_states
-        print(domain.action_names)
-        if not self.answered:
-            result[13] = 1.0
-            self.answered = True
-        else:
-            self.answered = False
-        print(result)
-        return self._prediction(result)
-        """Predicts the next action the bot should take after seeing the tracker.
+        print(tracker.latest_message.intent.get('name'))
+        print(tracker.latest_action)
+        response = "utter_saludar"
+        if tracker.latest_action['action_name'] == 'action_listen':
+            return self._prediction(confidence_scores_for(response, 1.0, domain))
+        if tracker.last_executed_action_has(response, skip=0):
+            return self._prediction(confidence_scores_for("action_listen", 1.0, domain))
+        return self._prediction(self._default_predictions(domain))
 
-        Args:
-            tracker: the :class:`rasa.core.trackers.DialogueStateTracker`
-            domain: the :class:`rasa.shared.core.domain.Domain`
-            interpreter: Interpreter which may be used by the policies to create
-                additional features.
-
-        Returns:
-             The policy's prediction (e.g. the probabilities for the actions).
-        """
-
-    def _metadata(self) -> Dict[Text, Any]:   
+    def _metadata(self) -> Dict[Text, Any]:
         return {
             "priority": self.priority,
+            "story_profiles": self.story_profiles
         }
-        
+
     @classmethod
     def _metadata_filename(cls) -> Text:
         return "test_policy.json"
